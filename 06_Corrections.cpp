19e0a6bb80bdfcd71b7a42ffaf6b434a12c342ee
William Rao Fernandes <williamraofernandes@gmail.com>
2025-11-12 17:16:38 +0100

    Update 06_Corrections.cpp

--- 06_Corrections.cpp
+++ 06_Corrections.cpp
@@ -10,6 +10,9 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     wc.lpfnWndProc = WndProc;             // Fonction de traitement des messages
     wc.hInstance = hInstance;             // Instance de l'application
     wc.lpszClassName = L"MainWindow";     // Nom unique de la classe
+
+
+    // EXERCICE 1
     wc.hbrBackground = CreateSolidBrush(RGB(0,0,200)); // Couleur de fond par défaut (blanche)
 
     // 2️ Enregistrer la classe auprès du système
@@ -44,10 +47,18 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     return (int)msg.wParam;
 }
 
+static int windowWidth;
+static int windowHeight;
+// EXERCICE 6
+static BOOL  g_dragging = FALSE;
+static POINT g_startMouse = {};   // position souris (écran) au clic
+static RECT  g_startWnd = {};   // rect fenêtre (écran) au clic
+// EXERCICE 7
+static int g_colorIndex = 0;
+static HBRUSH g_bgBrush = NULL;
+
 // Fonction de traitement des messages (callback)
 LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
-    static int windowWidth;
-    static int windowHeight;
     switch (uMsg) {
     case WM_CLOSE:
         DestroyWindow(hwnd);  // Quand l’utilisateur ferme la fenêtre
@@ -57,7 +68,7 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
         PostQuitMessage(0);   // Termine la boucle principale
         break;
 
-    // Titre qui change lors de la redimension de la fenetre 
+    // EXERCICE 2
     case WM_SIZE: {
         windowWidth = LOWORD(lParam);
         windowHeight = HIWORD(lParam);
@@ -67,29 +78,81 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
         break;
     }
 
-    // Changement de curseur
     case WM_MOUSEMOVE: {
+        // EXERCICE 3
         int x = LOWORD(lParam);
         int y = HIWORD(lParam);
 
         if (x < windowWidth / 2) SetCursor(LoadCursor(NULL, IDC_HAND));
-        else SetCursor(LoadCursor(NULL, IDC_CROSS));
+        else SetCursor(LoadCursor(NULL, IDC_CROSS)); 
+        // EXERCICE 6
+        if (g_dragging && (wParam & MK_LBUTTON)) {
+            POINT cur;
+            GetCursorPos(&cur);
+
+            int dx = cur.x - g_startMouse.x;
+            int dy = cur.y - g_startMouse.y;
+
+            int newX = g_startWnd.left + dx;
+            int newY = g_startWnd.top + dy;
+            int w = g_startWnd.right - g_startWnd.left;
+            int h = g_startWnd.bottom - g_startWnd.top;
+
+            SetWindowPos(hwnd, NULL, newX, newY, w, h, SWP_NOZORDER | SWP_NOACTIVATE);
+        }
+        return 0;
     }
 
-    // Affichage de la dernière touche pressée
     case WM_KEYDOWN: {
+        // EXERCICE 4
         if (wParam == VK_ESCAPE) {
             PostMessage(hwnd, WM_CLOSE, 0, 0);
             return 0;
         }
-
-        wchar_t keyName[64] = L"";
-        GetKeyNameText((LONG)lParam, keyName, 64);
-        wchar_t title[128];
-        wsprintf(title, L"Derniere touche: %s", keyName);
-        SetWindowText(hwnd, title);
+        //wchar_t keyName[64] = L"";
+        //GetKeyNameText((LONG)lParam, keyName, 64);
+        //wchar_t title[128];
+        //wsprintf(title, L"Derniere touche: %s", keyName);
+        //SetWindowText(hwnd, title);
         break;
     }
+    case WM_LBUTTONDOWN: {
+        // EXERCICE 5
+        //int x = LOWORD(lParam);
+        //int y = HIWORD(lParam);
+        //wchar_t msgText[128];
+        //wsprintf(msgText, L"Clic en (%d, %d)", x, y);
+        //MessageBox(hwnd, msgText, L"Info", MB_OK);
+
+        // EXERCICE 7
+        g_colorIndex = (g_colorIndex + 1) % 3;
+        COLORREF c = (g_colorIndex == 0) ? RGB(255, 0, 0) :
+            (g_colorIndex == 1) ? RGB(0, 255, 0) : RGB(0, 0, 255);
+        g_bgBrush = CreateSolidBrush(c);
+        InvalidateRect(hwnd, NULL, TRUE);
+
+        // EXERCICE 6
+        g_dragging = TRUE;
+        GetCursorPos(&g_startMouse);     
+        GetWindowRect(hwnd, &g_startWnd);
+
+        return 0;
+    }
+    // EXERCICE 6
+    case WM_LBUTTONUP:
+    {
+        if (g_dragging) {
+            g_dragging = FALSE;
+        }
+        return 0;
+    }
+
+    case WM_ERASEBKGND: {
+        RECT rc; GetClientRect(hwnd, &rc);
+        FillRect((HDC)wParam, &rc, g_bgBrush);
+        return 1;
+    }
+
     default:
         return DefWindowProc(hwnd, uMsg, wParam, lParam); // Traitement par défaut
     }
